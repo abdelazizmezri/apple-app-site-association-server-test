34d91855f7f4a1a168431d7fa4aec57589b34c78
Abdelaziz Mezri <amezri@fuzyo.com>
2024-06-21 23:20:17 +0100

    add apple file .well and source

--- server.js
+++ server.js
@@ -10,12 +10,26 @@ app.use((req, res, next) => {
   if (req.url === '/.well-known/apple-app-site-association') {
     res.setHeader('Content-Type', 'application/json');
   }
+  if (req.url === '/apple-app-site-association') {
+    res.setHeader('Content-Type', 'application/json');
+  }
   next();
 });
 
 // Route to serve the apple-app-site-association file
 app.get('/.well-known/apple-app-site-association', (req, res) => {
   const filePath = path.join(__dirname, '.well-known/apple-app-site-association');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading file');
+      return;
+    }
+    res.send(data);
+  });
+});
+
+app.get('/apple-app-site-association', (req, res) => {
+  const filePath = path.join(__dirname, 'apple-app-site-association');
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       res.status(500).send('Error reading file');
